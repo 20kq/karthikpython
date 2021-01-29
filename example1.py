c7a36b9c51c18894fd2dac3e6134f80dca53c876
20kq <uppugandla221@gmail.com>
2021-01-29 15:14:44 +0530

    first comment

---
+++ example1.py
@@ -0,0 +1,11 @@
+a=2
+b=5
+print('addition value is:-',a+b)
+print('subtraction value is :-',a-b)
+print('multiple value is:-',a*b)
+print('division value is:-',a/b)
+print('power value is:_',a**b)
+print('percentage value is:-',a%b)
+print('END')
+      
+      
